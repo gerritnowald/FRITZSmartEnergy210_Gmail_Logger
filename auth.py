9904b28e3676346c7a0898ae41f8d6efbaf14351
Gerrit Nowald <gerrit.nowald@gmail.com>
2025-07-04 10:16:29 +0200

    removed old code, changed scope, simplified

--- auth.py
+++ auth.py
@@ -1,16 +1,17 @@
+# author: Simonas Viliūnas  
+# https://github.com/vilisimo/examples/tree/main/gmail
+
 import os.path
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from pathlib import Path
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/gmail.readonly", "https://mail.google.com/"]
+SCOPES = ["https://www.googleapis.com/auth/gmail.modify"]
 
-FILE_PATH = os.path.dirname(os.path.realpath(__file__))
-TOKEN_PATH = str(Path(FILE_PATH, "token.json"))
-CREDENTIALS_PATH = str(Path(FILE_PATH, "credentials.json"))
+TOKEN_PATH = "token.json"
+CREDENTIALS_PATH = "credentials.json"
 
 
 def authenticate():
