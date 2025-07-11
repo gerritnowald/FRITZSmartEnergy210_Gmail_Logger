22ec36f61b4ded9beca089380d3b48cd48a7694f
Gerrit Nowald <gerrit.nowald@gmail.com>
2025-07-11 22:09:21 +0200

    added analysis, reduced nesting

--- get_power_data.py
+++ get_power_data.py
@@ -26,23 +26,25 @@ def DownloadAttachments(service, msg_id):
     message = service.users().messages().get(userId="me", id=msg_id).execute()
 
     for part in message['payload']['parts']:
-        if part['filename']:
-            if 'data' in part['body']:
-                data = part['body']['data']
-            else:
-                att_id = part['body']['attachmentId']
-                att = service.users().messages().attachments().get(userId="me", messageId=msg_id,id=att_id).execute()
-                data = att['data']
-            file_data = base64.urlsafe_b64decode(data.encode('UTF-8'))
+        if not part['filename']:
+            return
+        
+        if 'data' in part['body']:
+            data = part['body']['data']
+        else:
+            att_id = part['body']['attachmentId']
+            att = service.users().messages().attachments().get(userId="me", messageId=msg_id,id=att_id).execute()
+            data = att['data']
+        file_data = base64.urlsafe_b64decode(data.encode('UTF-8'))
 
-            path = os.path.join(data_dir, part['filename'])
-            if not os.path.exists(data_dir):
-                os.makedirs(data_dir)
-                print(f"Created directory {data_dir}")
+        path = os.path.join(data_dir, part['filename'])
+        if not os.path.exists(data_dir):
+            os.makedirs(data_dir)
+            print(f"Created directory {data_dir}")
 
-            with open(path, 'wb') as f:
-                f.write(file_data)
-                print(f"Downloaded {part['filename']} to {data_dir}")
+        with open(path, 'wb') as f:
+            f.write(file_data)
+            print(f"Downloaded {part['filename']} to {data_dir}")
 
 # -------------------------------------------------------------
 # function to download inline images from Gmail messages
---
+++ start_download.bat
@@ -0,0 +1,5 @@
+call python get_power_data.py ^
+    Label_231893078114603930 ^
+    --data_dir .\data
+
+PAUSE
