4a14cae9618b3a9daa1bb40ed76054eaf553a461
Gerrit Nowald <gerrit.nowald@3ds.com>
2025-07-15 12:20:13 +0200

    refactoring, removed functions

--- get_power_data.py
+++ get_power_data.py
@@ -16,86 +16,83 @@ argparser.add_argument("-d", "--data_dir", type=str, default="./attachments",
 argparser.add_argument("-t", "--trash", action='store_true',
                        help="Move messages to trash after downloading.")
 
-label_id = argparser.parse_args().labelID
+labelId  = argparser.parse_args().labelID
 data_dir = argparser.parse_args().data_dir
 trash    = argparser.parse_args().trash
 
 # -------------------------------------------------------------
-# functions to download attachments from Gmail messages
-# https://stackoverflow.com/questions/25832631/download-attachments-from-gmail-using-gmail-api
-
-def DownloadAttachments(service, msg_id, att_id, filename):
-    att = service.users().messages().attachments().get(userId="me", messageId=msg_id,id=att_id).execute()
-    file_data = base64.urlsafe_b64decode(att['data'].encode('UTF-8'))
-
-    path = os.path.join(data_dir, filename)
-    with open(path, 'wb') as f:
-        f.write(file_data)
-        print(f"Downloaded {filename} to {data_dir}")
-
-
-def get_attachment_ID(service, msg_id, message):
-    for part in message['payload']['parts']:
-        if not part['filename']:
-            continue
-
-        att_id = part['body']['attachmentId']
-        filename = part['filename']
-        
-        DownloadAttachments(service, msg_id, att_id, filename)
-
-
-def get_inline_ID(service, msg_id, message):
-    att_id   = message['payload']['parts'][0]['parts'][2]['body']['attachmentId']
-    filename = message['payload']['parts'][1]['filename'][:-4] + "_temperature.png"
-
-    DownloadAttachments(service, msg_id, att_id, filename)
-
-# -------------------------------------------------------------
 # authenticate
 
-creds = authenticate()
+creds   = authenticate()
 service = build("gmail", "v1", credentials=creds)
 
 # -------------------------------------------------------------
 # get message ids with specific label
 
-msg_ids = ( service.users().messages()
-    .list(userId="me", labelIds=[label_id]).execute() )
+msgIds = ( service.users().messages()
+    .list(userId="me", labelIds=[labelId]).execute() )
 
 try:
-    msg_ids = [msg['id'] for msg in msg_ids['messages']]
-    print(f"{len(msg_ids)} messages found")
+    msgIds = [msg['id'] for msg in msgIds['messages']]
+    print(f"{len(msgIds)} messages found")
 except KeyError:
-    msg_ids = []
+    msgIds = []
     print("No messages found")
 
 # -------------------------------------------------------------
-# download attachments and move messages to trash
+# create data directory if it doesn't exist
 
 if not os.path.exists(data_dir):
-        os.makedirs(data_dir)
-        print(f"Created directory {data_dir}")
+    os.makedirs(data_dir)
+    print(f"Created directory {data_dir}")
 
+# -------------------------------------------------------------
+# get messages
 
-for id in msg_ids:
+for msgId in msgIds:
     print(' ')
+    message = service.users().messages().get(userId="me", id=msgId).execute()
 
-    message = service.users().messages().get(userId="me", id=id).execute()
+# -------------------------------------------------------------
+# get attachments ids
 
+    attIds    = []
+    filenames = []
+    for part in message['payload']['parts']:
+        if part['filename']:
+            attIds.append(part['body']['attachmentId'])
+            filenames.append(part['filename'])
+        try:
+            for part in part['parts']:
+                if part['filename']:
+                    attIds.append(part['body']['attachmentId'])
+                    filenames.append(part['filename'])
+        except KeyError:
+            pass
 
-    get_attachment_ID(service, id, message)
+# -------------------------------------------------------------
+# download attachments
+# https://stackoverflow.com/questions/25832631/download-attachments-from-gmail-using-gmail-api
 
-    get_inline_ID(service, id, message)
+    for attId, filename in zip(attIds, filenames):
+        att = (service.users().messages().
+            attachments().get(userId="me", messageId=msgId, id=attId).execute() )
+        
+        file_data = base64.urlsafe_b64decode(att['data'].encode('UTF-8'))
 
+        path = os.path.join(data_dir, filename)
+        with open(path, 'wb') as f:
+            f.write(file_data)
+            print(f"Downloaded {filename} to {data_dir}")
 
-    if not trash:
-        continue
-    
-    for header in message['payload']['headers']:
-        if header['name'] == 'Subject':
-            subject = header['value']
-            break
+# -------------------------------------------------------------
+# move messages to trash
 
-    service.users().messages().trash(userId="me", id=id).execute()
-    print(f"Message '{subject}' moved to trash")
+    if trash:    
+        for header in message['payload']['headers']:
+            if header['name'] == 'Subject':
+                subject = header['value']
+                break
+
+        service.users().messages().trash(userId="me", id=msgId).execute()
+        print(f"Message '{subject}' moved to trash")
