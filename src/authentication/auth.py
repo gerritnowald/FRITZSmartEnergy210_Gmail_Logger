5cb65310d66b13076f7aec7b2b097c98dca94062
Gerrit Nowald <gerrit.nowald@gmail.com>
2025-08-28 09:44:07 +0200

    refresh token fixed

--- src/authentication/auth.py
+++ src/authentication/auth.py
@@ -3,6 +3,7 @@
 
 import os.path
 
+from google.auth.exceptions import RefreshError
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -27,7 +28,11 @@ def authenticate():
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+            try:
+                creds.refresh(Request())
+            except RefreshError:
+                os.remove(TOKEN_PATH)
+                return authenticate()
         else:
             flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_PATH, SCOPES)
             creds = flow.run_local_server(port=0)
